1b2c769d47b741a388cc54a2a0fdb9b40725e076
jaredolmosmb <jared@mb.company>
2022-05-10 21:36:09 -0600

    primera prueba para reunion del miercoles 11 de mayo

--- Thincrs_EducacionApp/Cursos/views.py
+++ Thincrs_EducacionApp/Cursos/views.py
@@ -319,13 +319,19 @@ def CargaTrayectoriaView(request):
             #comparacion de archivo nuevo de trayectorias con todas las preguntas precargadas
             for index, element in df.iterrows():#recorro lista para comparacion de elemento con los otros de la lista
               for index2, element2 in df2.iterrows():#recorro lista de conjunto total de preguntas
-                if index<index2: # asegurar que no se compara de manera reciproca
+                #if index == 16 and index2 == 522:
+                #if index<index2: # asegurar que no se compara de manera reciproca
                   if element[0] == element2[4]:# verifico que compara solo preguntas con el mismo ID:
                     probabilidad_similitud = similar(element[12], markdownify.markdownify(element2[5]).replace("\n", "").replace("  "," ")) # saque nivel de similitud de descripciones
+
                     if probabilidad_similitud >0.9 and probabilidad_similitud < 1.0:# si se sospecha de una descripcion que pudiera ser la misma se marca como invalido para carga ra bd
-                      valido2 = False
-                      print("La descripcion " +str(index)+ " es similar en un: " +str(probabilidad_similitud)+ " de la descripción " +str(index2)+ "podria tratarse de la misma pregunta. Favor de revisar  " +element[0])# se indica los renglones a revisar
-            if valido:
+                        print("descripcion "+str(index+2)+" en preguntas2: ")
+                        print(element[12])
+                        print("descripcion "+str(index2+2)+" en out: ")
+                        print(markdownify.markdownify(element2[5]).replace("\n", "").replace("  "," "))
+                        valido2 = False
+                        print("La descripcion " +str(index+2)+ " es similar en un: " +str(probabilidad_similitud)+ " de la descripción " +str(index2+2)+ " podria tratarse de la misma pregunta. Favor de revisar  " +element[0])# se indica los renglones a revisar
+            if valido2:
               print("La nueva trayectoria es correcta cargar a BD")
             else:
               print("checar archivo de excel hay posible repeticion con alguna(s) pregunta(s) en la BD")
